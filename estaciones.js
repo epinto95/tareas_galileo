07c7e4a6f536e471598bf9d2e9a040c397eafe27
Ervin Pinto <ervin.pinto@galileo.edu>
2020-02-16 16:36:40 -0600

    Mi primer código en Git

---
+++ estaciones.js
@@ -0,0 +1,19 @@
+var mes=10;
+var nombre="Ervin Antonio Pinto", carne="20002516";
+if (mes==1 || mes==2 || mes==12) {
+  document.write("Es invierno")
+}
+else if ((mes>=3) && (mes<=5)) {
+  document.write("Es Primavera");
+}
+else if ((mes>=6) && (mes<=8)) {
+  document.write("Es Verano");
+}
+else if ((mes>=9) && (mes<=11)) {
+  document.write("Es Otoño");
+}
+else {
+  document.write("Intente con un numero entre 1 y 12");
+}
+document.writeln("<br/>" + nombre);
+document.writeln("<br/>" + carne);
